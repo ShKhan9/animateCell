947df1e0ee6b938cb50f9aa5ac9da31c9cbc1351
shehata <shehatagamal120@yahoo.com>
2017-12-25 16:15:10 +0200

    fsfasf

--- animateCell/ViewController.swift
+++ animateCell/ViewController.swift
@@ -176,7 +176,7 @@ class ViewController: UIViewController , UITableViewDataSource,UITableViewDelega
             
             if(cell?.animate)!
             {
-              UIView.animate(withDuration: 1.0, animations: {
+              UIView.animate(withDuration: 0.2, animations: {
                 
                 cell?.labeltopCon.constant = 20
                 
